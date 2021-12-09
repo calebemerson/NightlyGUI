a323676e34dbce15286354e12af2fd03f52d0f27
calebemerson <caleb.emerson@ufl.edu>
2021-12-09 12:08:05 -0500

    Nightly

---
+++ cmake-build-debug/CMakeFiles/NightlyGUI.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NightlyGUI.dir/TextureManager.cpp.obj"
+  "CMakeFiles/NightlyGUI.dir/main.cpp.obj"
+  "NightlyGUI.exe"
+  "NightlyGUI.exe.manifest"
+  "NightlyGUI.pdb"
+  "libNightlyGUI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NightlyGUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
